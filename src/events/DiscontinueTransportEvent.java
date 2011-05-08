c647780436efd4f27dc5d051a239f8948dd495ce
Nick Malcolm <nick@southgatelabs.com>
2011-05-09 11:34:23 +1200

    Adds fields, getters and constructor for Discontinue Transport Event

--- src/events/DiscontinueTransportEvent.java
+++ src/events/DiscontinueTransportEvent.java
@@ -1,5 +1,39 @@
 package events;
 
+import priority.Priority;
+import routes.DistributionCentre;
+import routes.Firm;
+
 public class DiscontinueTransportEvent extends Event {
+	
+	private Firm firm;
+	private Priority priority;
+	private DistributionCentre origin;
+	private DistributionCentre destination;
+	
+	
+	
+	public DiscontinueTransportEvent(Firm firm, Priority priority,
+			DistributionCentre origin, DistributionCentre destination) {
+		super();
+		this.firm = firm;
+		this.priority = priority;
+		this.origin = origin;
+		this.destination = destination;
+	}
+	
+	
+	public Firm getFirm() {
+		return firm;
+	}
+	public Priority getPriority() {
+		return priority;
+	}
+	public DistributionCentre getOrigin() {
+		return origin;
+	}
+	public DistributionCentre getDestination() {
+		return destination;
+	}
 
 }
