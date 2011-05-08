76fb2ef1c7d7567006e7c0cfc45b3044cb0b8f5c
Nick Malcolm <nick@southgatelabs.com>
2011-05-09 11:47:44 +1200

    Adds fields and constructor to PriceUpdate event

--- src/events/PriceUpdateEvent.java
+++ src/events/PriceUpdateEvent.java
@@ -1,5 +1,23 @@
 package events;
 
 public class PriceUpdateEvent extends Event {
+	
+	private double costPerCC;
+	private double costPerG;
+
+	public PriceUpdateEvent(double costPerCC, double costPerG) {
+		super();
+		this.costPerCC = costPerCC;
+		this.costPerG = costPerG;
+	}
+	
+	public double getCostPerCC() {
+		return costPerCC;
+	}
+	public double getCostPerG() {
+		return costPerG;
+	}
+	
+	
 
 }
