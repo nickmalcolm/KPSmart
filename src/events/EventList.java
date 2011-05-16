5c333b9c96b5b842c4a9dba5bdbabfe74e30aa70
Nick Malcolm <nick@southgatelabs.com>
2011-05-16 23:45:18 +1200

    Adds filtering method to EventList. Also now stores them in order Newest In Newest Out

--- src/events/EventList.java
+++ src/events/EventList.java
@@ -1,16 +1,49 @@
 package events;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class EventList<T extends Event> extends ArrayList<T>{
 	
 	private static final long serialVersionUID = 1L;
 
 	
-	private ArrayList<T> list;
+	private ArrayList<T> list = new ArrayList<T>();
+	
+	private boolean dirty = false;
 
-
-	public EventList() {
+	public EventList<T> showOnly(Class klass){
+		
+		EventList<T> newList = new EventList<T>();
+		
+		for(Event e : list){
+			if( e.getClass().isInstance(klass)){
+				newList.add((T) e);
+			}
+		}
+		
+		return newList;
+	}
+	
+	public boolean add(T event){
+		dirty = true;
+		return list.add(event);
+	}
+	
+	public T get(int index){
+		if(dirty){
+			Collections.sort(list);
+			dirty = false;
+		}
+		return list.get(index);
+	}
+	
+	public boolean contains(Object o){
+		return list.contains(o);
+	}
+	
+	public int getSize(){
+		return list.size();
 	}
 	
 }
